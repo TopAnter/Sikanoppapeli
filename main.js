ef7459d8f9391ab232326b957537461d7883d2cd
TopAnter <topsumikael.maki@gmail.com>
2025-05-08 19:14:35 +0300

    etee

--- main.js
+++ main.js
@@ -5,27 +5,36 @@ document.getElementById("pyorautus1").addEventListener("click", heitto)
 document.getElementById("lisaaPelaaja").addEventListener("click", pelLisays)
 document.getElementById("aloitusForm").addEventListener("submit", aloitaPeli)
 
+
 function heitto(event){
     event.preventDefault()
 }
 
 function pelLisays(event){
-    pelaajat.push(0)
-    let uusiP = document.createElement("p")
-    uusiP.textContent = "pelaaja " + (pelaajaMaara + 1) + ": " + document.getElementById("pelaajaNimi").value + " pisteet: " + pelaajat[pelaajaMaara]
-    uusiP.id = (pelaajaMaara + 1)
-    document.getElementById("pelaajaLista").append(uusiP)
-    pelaajaMaara = pelaajaMaara + 1
     event.preventDefault()
+    if(document.getElementById("pelaajaNimi").value.length != 0){
+        pelaajat.push(0)
+        let uusiP = document.createElement("p")
+        uusiP.textContent = "pelaaja " + (pelaajaMaara + 1) + ": " + document.getElementById("pelaajaNimi").value + " pisteet: " + pelaajat[pelaajaMaara]
+        uusiP.id = (pelaajaMaara + 1)
+        document.getElementById("pelaajaLista").append(uusiP)
+        pelaajaMaara = pelaajaMaara + 1
+    }
 }
 
 function aloitaPeli(event){
-    if(pelaajat.length() > 1){
-        if(document.getElementById("versioValinta").value = 1){
+    event.preventDefault()
+
+    let pisteTavoite = document.getElementById("pisteTavoite").value
+    let versio = document.getElementById("versioValinta").value
+
+    if(pelaajat.length > 1){
+        if(versio == 1){
             document.getElementById("peli1").style.display = "block"
         }
-    }else{
+    }else if(pisteTavoite == 0 || pisteTavoite.trim() == ""){
+        document.getElementById("ilmoitus").innerText = "Aseta pisteTavoite"
+    }else if(pelaajat.length <= 1){
         document.getElementById("ilmoitus").innerText = "Ei tarpeeksi pelaajia (2 vaadittu)"
     }
-    event.preventDefault()
 }
